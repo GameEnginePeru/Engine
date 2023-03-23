31b8484b60c0537e9e458f190709afe423d6a4ef
Marcelo Colonia <31974620+HalloweenJack2606@users.noreply.github.com>
2023-03-22 21:52:05 -0500

    Added Json utils

--- Engine/src/Core/CoreSTD.h
+++ Engine/src/Core/CoreSTD.h
@@ -1,17 +1,23 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
+#include <fstream>
+
+#include <functional>
+
 #include <string>
 #include <map>
 #include <vector>
-#include <functional>
-#include <sstream>
-#include <fstream>
+#include <unordered_map>
 
 typedef std::string CString;
 
 template<typename Key, typename Value>
 class CMap : public std::map<Key, Value> {};
+
+template<typename Key, typename Value>
+class CUnorderedMap : public std::map<Key, Value> {};
 
 template<typename T>
 class CVector : public std::vector<T> {};
---
+++ Engine/src/Utils/Json/Json.h
@@ -0,0 +1,68 @@
+#pragma once
+#include "Core/CoreSTD.h"
+
+namespace Engine
+{
+    enum class JSONType {
+        NULL_TYPE,
+        BOOL_TYPE,
+        NUMBER_TYPE,
+        STRING_TYPE,
+        ARRAY_TYPE,
+        OBJECT_TYPE
+    };
+
+    class JSON {
+    public:
+        JSON() : m_Type(JSONType::NULL_TYPE), m_Value("") {}
+        JSON(bool b) : m_Type(JSONType::BOOL_TYPE), m_Value(b ? "true" : "false") {}
+        JSON(double n) : m_Type(JSONType::NUMBER_TYPE), m_Value(std::to_string(n)) {}
+        JSON(const CString& s) : m_Type(JSONType::STRING_TYPE), m_Value(s) {}
+        JSON(CVector<JSON> v) : m_Type(JSONType::ARRAY_TYPE), m_Array(v) {}
+        JSON(CUnorderedMap<CString, JSON> m) : m_Type(JSONType::OBJECT_TYPE), m_Object(m) {}
+
+        JSONType GetType() const { return m_Type; }
+        bool IsNull() const { return m_Type == JSONType::NULL_TYPE; }
+        bool IsBool() const { return m_Type == JSONType::BOOL_TYPE; }
+        bool IsNumber() const { return m_Type == JSONType::NUMBER_TYPE; }
+        bool IsString() const { return m_Type == JSONType::STRING_TYPE; }
+        bool IsArray() const { return m_Type == JSONType::ARRAY_TYPE; }
+        bool IsObject() const { return m_Type == JSONType::OBJECT_TYPE; }
+
+        bool AsBool() const { return m_Value == "true"; }
+        double AsNumber() const { return std::stod(m_Value); }
+        CString AsString() const { return m_Value; }
+        CVector<JSON> AsArray() const { return m_Array; }
+        CUnorderedMap<CString, JSON> AsObject() const { return m_Object; }
+
+        CString EscapeString(const CString& input) const;
+
+        static JSON Parse(const CString& json);
+        void Print(std::ostream& os) const;
+
+        JSON& operator[](const CString& key) {
+            if (m_Type == JSONType::OBJECT_TYPE) return m_Object[key];
+            else throw std::runtime_error("Not an Object");
+        }
+
+        const JSON& operator[](const CString& key) const {
+            if (m_Type == JSONType::OBJECT_TYPE) {
+                auto it = m_Object.find(key);
+                if (it != m_Object.end()) {
+                    return it->second;
+                }
+                else {
+                    throw std::runtime_error("Key not found");
+                }
+            }
+            else {
+                throw std::runtime_error("Not an object");
+            }
+        }
+    private:
+        JSONType m_Type;
+        CString m_Value;
+        CVector<JSON> m_Array;
+        CUnorderedMap<CString, JSON> m_Object;
+    };
+}
