0b283de5f87eae33a05cf652968d2a44ca7898d8
Marcelo Colonia <31974620+HalloweenJack2606@users.noreply.github.com>
2023-03-27 03:42:41 -0500

    Fixed boilerplate WindowsWindow code

--- Engine/src/Core/Window/Window.h
+++ Engine/src/Core/Window/Window.h
@@ -21,7 +21,7 @@ namespace ENGINE_NAMESPACE
 		virtual uint32 GetWidth() const = 0;
 		virtual uint32 GetHeight() const = 0;
 
-		virtual void SetVSync(bool enabled) = 0;
+		virtual void SetVSync(bool bEnabled) = 0;
 		virtual bool IsVSync() const = 0;
 
 		virtual void* GetNativeWindow() const = 0;
--- Engine/src/Platform/WindowsPlatform/Window/WindowsWindow.h
+++ Engine/src/Platform/WindowsPlatform/Window/WindowsWindow.h
@@ -6,5 +6,28 @@ namespace ENGINE_NAMESPACE
 	class WindowsWindow : public Window
 	{
 	public:
+		WindowsWindow(const WindowProperties& props) {}
+		virtual ~WindowsWindow() override {}
+
+		virtual void OnUpdate() override {}
+		
+		virtual uint32 GetWidth() const override { return m_Data.uWidth; }
+		virtual uint32 GetHeight() const override { return m_Data.uHeight; }
+
+		virtual void SetVSync(bool bEnabled) override {}
+		virtual bool IsVSync() const override { return m_Data.bVSync; }
+
+		// @TODO: change!
+		virtual void* GetNativeWindow() const override { return nullptr; }
+	private:
+		struct WindowData
+		{
+			CString title;
+			uint32 uWidth = 0, uHeight = 0;
+			bool bVSync = true;
+			bool bResizable = false;
+			bool bMaximized = false;
+			uint32 uSamples = 0;
+		} m_Data;
 	};
 }
